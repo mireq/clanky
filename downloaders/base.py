2cac7401dcfbcb651168e5d5b6d78a0bd3b9bfb0
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-29 05:55:02 +0100

    feat: Added link extractor based on attrs

--- downloaders/base.py
+++ downloaders/base.py
@@ -3,9 +3,17 @@ from urllib.parse import urlparse
 
 import requests
 from lxml import html
+from typing import Set
+
+
+class DownloadError(RuntimeError):
+	pass
 
 
 class BaseDownloader(object):
+	LINK_ATTRS = {'href', 'src'}
+	ARTICLE_XPATH = 'body/main'
+
 	def __init__(self, name):
 		self.name = name
 		self.kwargs = {}
@@ -24,6 +32,7 @@ class BaseDownloader(object):
 		self.kwargs = kwargs
 		self.main_document = self.download_main_document()
 		self.main_document_tree = self.parse_main_document()
+		self.links = self.extract_links()
 
 	def download_main_document(self):
 		req = requests.get(self.kwargs['url'])
@@ -41,3 +50,17 @@ class BaseDownloader(object):
 		tree = html.fromstring(self.main_document, base_url=base_url)
 		tree.make_links_absolute()
 		return tree
+
+	def extract_links(self):
+		main_content = self.main_document_tree.find(self.ARTICLE_XPATH)
+		if main_content is None:
+			raise DownloadError(f"Element {self.ARTICLE_XPATH} not found")
+		candidates = set()
+		candidates |= self.find_fragment_links(main_content)
+		return candidates
+
+	def find_fragment_links(self, fragment) -> Set[str]:
+		links = set()
+		for attr in self.LINK_ATTRS:
+			links |= set(element.attrib[attr] for element in fragment.iterfind(f'.//*[@{attr}]'))
+		return links
