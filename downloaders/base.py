b0c72b26796038b190694d650ab2b6070b046faf
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-29 05:34:02 +0100

    feat: Parse tree, make links absolute

--- downloaders/base.py
+++ downloaders/base.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from urllib.parse import urlparse
-import requests
 
+import requests
+from lxml import html
 
 
 class BaseDownloader(object):
@@ -9,6 +10,7 @@ class BaseDownloader(object):
 		self.name = name
 		self.kwargs = {}
 		self.main_document = ''
+		self.main_document_tree = None
 
 	def add_arguments(self, parser):
 		pass
@@ -17,11 +19,25 @@ class BaseDownloader(object):
 	def parsed_url(self):
 		return urlparse(self.kwargs['url'])
 
+
 	def handle_download(self, **kwargs):
 		self.kwargs = kwargs
 		self.main_document = self.download_main_document()
+		self.main_document_tree = self.parse_main_document()
 
 	def download_main_document(self):
 		req = requests.get(self.kwargs['url'])
 		req.raise_for_status()
 		return req.text
+
+	def parse_main_document(self):
+		tree = html.fromstring(self.main_document)
+		base_url = None
+		basetags = tree.xpath('//base[@href]|//x:base[@href]', namespaces={'x': html.XHTML_NAMESPACE})
+		for b in basetags:
+			base_url = b.get('href')
+		if base_url is None:
+			base_url = self.kwargs['url']
+		tree = html.fromstring(self.main_document, base_url=base_url)
+		tree.make_links_absolute()
+		return tree
