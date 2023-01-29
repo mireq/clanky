5b9badb0e7247154c76dbb8666bac4129ae26f6e
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-29 05:23:18 +0100

    feat: Download main document

--- downloaders/base.py
+++ downloaders/base.py
@@ -1,10 +1,27 @@
 # -*- coding: utf-8 -*-
+from urllib.parse import urlparse
+import requests
+
+
+
 class BaseDownloader(object):
 	def __init__(self, name):
 		self.name = name
+		self.kwargs = {}
+		self.main_document = ''
 
 	def add_arguments(self, parser):
 		pass
 
-	def handle_download(self, **args):
-		print(args)
+	@property
+	def parsed_url(self):
+		return urlparse(self.kwargs['url'])
+
+	def handle_download(self, **kwargs):
+		self.kwargs = kwargs
+		self.main_document = self.download_main_document()
+
+	def download_main_document(self):
+		req = requests.get(self.kwargs['url'])
+		req.raise_for_status()
+		return req.text
