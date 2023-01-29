f048d0a9da72c609f4b9ad394edc8d3e74c9c11b
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-01-29 16:23:34 +0100

    feat: Removed sharer from code

--- downloaders/wisdom_blog.py
+++ downloaders/wisdom_blog.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from .base import BaseDownloader
 
+# https://eu-central-1.linodeobjects.com/wisdomtech/media/filer_public_thumbnails/filer_public/66/7d/667d69dd-d314-4753-97eb-813ebfc549cb/pisanie_ciarok.png__1600x1100_subsampling-2.png
+# https://eu-central-1.linodeobjects.com/wisdomtech/media/filer_public/66/7d/667d69dd-d314-4753-97eb-813ebfc549cb/pisanie_ciarok.png
+
 
 class Downloader(BaseDownloader):
-	pass
+	ARTICLE_XPATH = 'body/main/div[@class="main-text"]'
+
+	def parse_main_document(self):
+		doc = super().parse_main_document()
+		sharer = doc.find('.//div[@class="share"]')
+		if sharer is not None:
+			sharer.getparent().remove(sharer)
+		return doc
