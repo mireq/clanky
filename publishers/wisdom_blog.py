857f06f5733b55dc2e2d0295ce8d562b6bf1aba9
Miroslav Bendík <miroslav.bendik@gmail.com>
2023-02-04 09:28:41 +0100

    feat: Processing uploads

--- publishers/wisdom_blog.py
+++ publishers/wisdom_blog.py
@@ -20,7 +20,8 @@ class Publisher(BasePublisher):
 		self.load_cookies(section=server)
 		self.do_login()
 		self.article_id = self.get_or_create_article_id()
-		print(self.article_id)
+		self.attachments_element, self.gallery_element = self.get_library_elements()
+		file_mapping = self.update_files()
 
 	def is_logged(self):
 		response = self.session.get(self.server + 'dashboard/blog/blogpost/')
@@ -90,3 +91,73 @@ class Publisher(BasePublisher):
 			article_id = re.match(r'^/dashboard/blog/blogpost/(\d+)/change/$', path).group(1)
 			self.set_metadata('id', article_id, section=self.server)
 		return article_id
+
+	def extract_files(self):
+		other_files = super().extract_files()
+		files = {}
+		body = self.article.doc.find('body/article')
+		candidate = body.attrib.get('data-title-image')
+		if candidate is not None:
+			try:
+				file_info = self.get_file_info(candidate)
+				files[file_info.name] = self.get_file_info(candidate)
+			except FileNotFoundError as e:
+				sys.stderr.write(f"File does not exist {e.filename}\n")
+			except Exception:
+				traceback.print_exc()
+		files.update(other_files)
+		return files
+
+	def decode_library_element(self, element) -> dict:
+		return {
+			'value': element.attrib['value'],
+			'list_url': element.attrib['data-list-url'],
+			'upload_url': element.attrib['data-upload-url'],
+			'update_url': element.attrib['data-update-url'],
+			'create_library_url': element.attrib['data-create-library-url'],
+		}
+
+	def get_library_elements(self):
+		update_url = f'{self.server}dashboard/blog/blogpost/{self.article_id}/change/'
+		response = self.session.get(update_url)
+		response.raise_for_status()
+		html_doc = etree.fromstring(response.text, etree.HTMLParser())
+		attachments = html_doc.find('.//input[@name="attachments"]')
+		gallery = html_doc.find('.//input[@name="gallery"]')
+		return self.decode_library_element(attachments), self.decode_library_element(gallery)
+
+	def update_files(self):
+		print(self.attachments_element)
+		uploaded_files = self.get_uploaded_fiels()
+		to_delete_hashes = set(uploaded_files.keys()) - set(info.hash for info in self.files.values())
+
+		try:
+			# TODO: implement
+			#self.delete_files(uploaded_files[h] for h in to_delete_hashes)
+			pass
+		except Exception:
+			traceback.print_exc()
+		for h in to_delete_hashes:
+			del uploaded_files[h]
+
+		to_upload_files = [info for info in self.files.values() if not info.hash in uploaded_files]
+		try:
+			uploaded_files.update(self.upload_files(to_upload_files))
+		except Exception:
+			traceback.print_exc()
+		self.set_metadata('uploads', json.dumps(uploaded_files), section=self.server)
+
+		file_mapping = []
+		for info in self.files.values():
+			uploaded = uploaded_files.get(info.hash)
+			if uploaded is not None:
+				file_mapping.append([info, uploaded])
+		return file_mapping
+
+	def upload_files(self, files):
+		files = list(files)
+		if not files:
+			return {}
+
+		upload_metadata = {}
+		return upload_metadata
